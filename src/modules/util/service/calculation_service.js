05ff188afe253d216ed9a03fa5066efaa9f6d012
mrugesh08 <mrugesh@whitehatjr.com>
2019-12-01 04:32:18 +0530

    changes

--- src/modules/util/service/calculation_service.js
+++ src/modules/util/service/calculation_service.js
@@ -7,13 +7,14 @@ module.exports = define('calculationService', () => {
     if (!currentPage || parseInt(currentPage) === 1) {
       pageInfo.offset = 0
       pageInfo.page = 1
+      pageInfo.nextPage = 2
     } else {
       pageInfo.offset = limit * parseInt(currentPage)
+      pageInfo.nextPage = parseInt(currentPage) + 1
       pageInfo.page = parseInt(currentPage) - 1
     }
 
     pageInfo.totalPages = Math.ceil(totalCount / limit)
-    pageInfo.nextPage = (pageInfo.page === 1) ? pageInfo.page + 1 : pageInfo.page + 2
     return pageInfo
   }
 
